7d16850a290f456c628faecb56bdd0a3b71991bb
Pavel Claudiu Stefan <pavelclaudiu96@gmail.com>
2017-04-14 03:39:14 +0300

    Initial project commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+/**
+ * Created by Claudiu on 14-Apr-17.
+ *
+ * Laser Tag
+ * Proiect PAO
+ *
+ * Pavel Claudiu Stefan
+ * Mihai George
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello, world!");
+
+    }
+
+}
